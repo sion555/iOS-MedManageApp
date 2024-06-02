7f4a4ba973f1d9869fff0b2f01241ea82f2110a5
sion <hbs09222@gmail.com>
2024-06-03 08:31:24 +0900

    Xcode Project File Commit

---
+++ iOS-MedManager/iOS-MedManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-MedManager
+//
+//  Created by 한범석 on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
