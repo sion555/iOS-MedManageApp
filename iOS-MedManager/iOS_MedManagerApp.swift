c126161a80c32e6f4659266c619690ab169b9ea7
이유경 <a260@iyugyeong-ui-MacBookPro.local>
2024-06-03 09:50:02 +0900

    Xcode uk commit

---
+++ iOS-MedManager/iOS_MedManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_MedManagerApp.swift
+//  iOS-MedManager
+//
+//  Created by 한범석 on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_MedManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
