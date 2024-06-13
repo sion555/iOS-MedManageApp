77c3e155566866568537f71bade6d1a488684291
sykr1313 <sykr1313@outlook.com>
2024-06-13 14:26:15 +0900

    3월 13일 약톡톡 UI

---
+++ iOS-MedManager/Views/PhotographView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotographView.swift
+//  iOS-MedManager
+//
+//  Created by ksy on 6/13/24.
+//
+
+import SwiftUI
+
+struct PhotographView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotographView()
+}
